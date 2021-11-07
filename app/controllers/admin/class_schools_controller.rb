03f439f508f8b44e20c2553222a911de634515cf
gessi <gessilane.i3solucoes@gmail.com>
2021-11-07 17:12:48 -0300

    Capacitação ruby on rails

---
+++ app/controllers/admin/class_schools_controller.rb
@@ -0,0 +1,55 @@
+class Admin::ClassSchoolsController < Admin::BaseController
+  before_action :set_class_school, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @class_schools = ClassSchool.all
+  end
+
+  def new
+    @class_school = ClassSchool.new
+  end
+
+  def edit
+    @class_school = ClassSchool.find(params[:id])
+  end
+
+  def show
+  end
+
+  def create
+    if @class_school.save
+      flash[:notice] = 'Turma criada com sucesso'
+      redirect_to admin_class_schools_path
+    else
+      flash[:alert] = 'Erro ao cadastrar turma, por favor corrigi.'
+      render :new
+    end
+  end
+
+  def update
+    if @class_school.update(class_school_params)
+      flash[:notice] = 'Turma atualizada com sucesso'
+      redirect_to admin_class_schools_path
+    else
+      flash[:alert] = 'Erro ao atualizar turma, por favor corrigi.'
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @class_school.destroy
+    flash[:notice] = 'Turma excluída com sucesso.'
+    redirect_to action: :index
+  end
+
+
+private
+  def set_class_school
+    @class_school = ClassSchool.find(params[:id])
+  end
+
+  def class_school_params
+    params.require(:class_school).permit(:name)
+  end
+end
